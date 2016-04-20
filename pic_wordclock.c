69ad2b0f48c73d06bbb85576560074c5e050a2ec
Marcel Eschmann <marcel.eschmann@gmail.com>
2016-04-20 18:52:41 +0200

    Add interrupt boilerplate

--- pic_wordclock.c
+++ pic_wordclock.c
@@ -1,4 +1,9 @@
+/* B Knudsen Cc5x C-compiler - not ANSI-C */
+#include "16F690.h"
+#include "int16Cxx.h"
+#pragma config |= 0x00D4
 
+/* General */
 #define LSBFIRST		1
 #define DATA_PORT 		PORTC.0
 #define CLOCK_PORT 		PORTC.1
@@ -7,6 +12,32 @@
 void shiftOut(char dataPin, char clockPin, char val);
 void initPorts(void);
 
+/**
+ * Interrupt service routine
+ */
+#pragma origin 4
+interrupt isr(void) {
+    int_save_registers
+    // interrupt routine here
+    //RABIF = 0;
+    int_restore_registers
+}
+
+/**
+ * Main routine
+ */
+void main( void)
+{
+    initPorts();
+    //IOCA.5 = 1; // interrupt on RA5 pin enable
+    //IOCA.4 = 1; // interrupt on RA4 pin enable
+    RABIE = 1; // local interrupt enable
+    GIE = 1; // global interrupt enable
+
+    while(1) {
+        // do something between interrupts
+    }
+}
 
 /**
  * Initialise PIC16F690 general purpose I/O ports.
@@ -16,7 +47,6 @@ void initPorts(void) {
 	TRISC.1 = 0;
 	TRISC.2 = 0;
 }
-
 
 /**
  * Shifts out a byte of data one bit at a time. Starts from the most significant bit. 
